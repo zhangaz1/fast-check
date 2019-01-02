46fa9e1a19b434e413bd2c3f1cfca5d357f374f8
Nicolas DUBIEN <github@dubien.org>
2019-01-02 21:56:40 +0100

    POC Switch to variadic generics for property and tuple
    Related to #154

--- src/check/arbitrary/TupleArbitrary.ts
+++ src/check/arbitrary/TupleArbitrary.ts
@@ -1,4 +1,10 @@
-import { tuple } from './TupleArbitrary.generated';
+import { Arbitrary } from './definition/Arbitrary';
 import { genericTuple } from './TupleArbitrary.generic';
+
+type ArbitraryTuple<Ts> = { [P in keyof Ts]: Arbitrary<Ts[P]> };
+
+function tuple<Ts extends any[]>(...arbs: ArbitraryTuple<Ts>): Arbitrary<Ts> {
+  return genericTuple(arbs) as any;
+}
 
 export { tuple, genericTuple };
--- src/check/property/Property.ts
+++ src/check/property/Property.ts
@@ -1,4 +1,14 @@
+import { Arbitrary } from '../arbitrary/definition/Arbitrary';
+import { tuple } from '../arbitrary/TupleArbitrary';
 import { property } from './Property.generated';
 import { Property } from './Property.generic';
+
+type ArbitraryTuple<Ts> = { [P in keyof Ts]: Arbitrary<Ts[P]> };
+type Prop<Ts extends any[]> = (...args: Ts) => boolean | void;
+
+// Signature change... :/
+export function property2<Ts extends any[]>(arbs: ArbitraryTuple<Ts>, predicate: Prop<Ts>): Property<Ts> {
+  return new Property(tuple<Ts>(...(arbs as any)), t => predicate(...t));
+}
 
 export { property, Property };
